f7d1605839ecdbd42ae4ca0011ddbc5fc8ddd144
Caleb Champion <cchampi@purdue.edu>
2025-05-19 15:39:34 -0400

    cache

---
+++ pipeline/RAG_model/rag_engine.py
@@ -0,0 +1,5 @@
+'''based on the context of the retreived slack messages, make 
+   a model response that answers the question. 
+   so the solution should look like 
+   AI answer = slack message augmented ai prompt'''
+
