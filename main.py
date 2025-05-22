a76b6b9876dfd29ff8084bedf233faefebd06faf
Caleb Champion <cchampi@purdue.edu>
2025-05-22 12:47:26 -0400

    working model with slackloader

--- main.py
+++ main.py
@@ -10,15 +10,11 @@ from pipeline.rag_engine import OllamaLLM
 
 #const
 DB_PATH = "database"
-SLACK_PATH = "data/slack/Megawatt Slack export May 1 2019 - May 19 2025"
+ZIP_PATH = "data/slack/mwzipp.zip"
+EMBEDDING_MODEL = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
 
 #main
 def main():
-  '''load preloaded ai embedding model
-  '''
-  embedding_model = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
-
-
 
   '''parses and indexes into vector db if needed
   '''
@@ -26,20 +22,20 @@ def main():
     print("\nDatabase already exists. Skipping indexing.\n")
 
   else: #make a database for it
-    indexer = SlackIndexer(slack_dir = SLACK_PATH, db_path = DB_PATH, embeddings = embedding_model)
+    indexer = SlackIndexer(slack_dir = ZIP_PATH, db_path = DB_PATH, embeddings = EMBEDDING_MODEL)
     indexer.create_vector_store() # store the vector database
 
 
 
   '''query and prompt ai model
   '''
-  RAG_searcher = VectorSearcher(db_path = DB_PATH, embeddings = embedding_model)  #load model object in beforehand so it doesnt load every time
+  RAG_searcher = VectorSearcher(db_path = DB_PATH, embeddings = EMBEDDING_MODEL)  #load model object in beforehand so it doesnt load every time
   while True:
     query = input("What can I help you with? -> ")
     if query.lower() == "exit":
       break
     top_k_results = RAG_searcher.search(query = query, top_k = 20) #searches and responds with top 10 k-map slack messages
-    print(f"\ntop 5 slack messages: \n\n{top_k_results}")
+    #print(f"\ntop 5 slack messages: \n\n{top_k_results}")
 
     LLM = OllamaLLM()  #pick from any model model = "llama3.2" as default
     stream = LLM.generate_with_context(query = query, recieved_data = top_k_results)
@@ -49,4 +45,3 @@ def main():
 #run
 if __name__ == "__main__":
   main()
-
--- pipeline/preprocessing/indexer.py
+++ pipeline/preprocessing/indexer.py
@@ -12,103 +12,33 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 #nothing else will change in the code
 from langchain_community.vectorstores import Chroma #same with Chroma, it will throw a warning to a new model, it should be langchain-Chroma in the future
 from langchain_core.documents import Document
+from langchain_community.document_loaders import SlackDirectoryLoader
 
 class SlackIndexer:
    def __init__(self, slack_dir: str, db_path: str,
                embeddings, #model from huggingface, (text->vector) with semantic meaning
+               google_dir: str = "data/google",
                chunk_size: int = 1000,  #customizable
                chunk_overlap: int = 200  #customizable
                ):
 
       self.slack_dir = slack_dir
+      self.google_dir = google_dir
       self.db_path = db_path
       self.chunk_size = chunk_size
       self.chunk_overlap = chunk_overlap
       self.embeddings = embeddings
       self.vectorstore = None  #vector database placeholder
 
-   def load_messages(self) -> List[Dict]:
-      #load messages from slack into a list of dictionaries
-      all_messages = []
-
-      for subdir in os.listdir(self.slack_dir):
-         if subdir.endswith(".json"):
-            continue
-         subdir_path = os.path.join(self.slack_dir, subdir)
-         if os.path.isdir(subdir_path):  # Ensure it's a folder
-            for filename in os.listdir(subdir_path):
-               if filename.endswith(".json"):
-                  path = os.path.join(subdir_path, filename)
-                  try:
-                     with open(path, "r", encoding="utf-8") as f:
-                        data = json.load(f)
-
-                        for message in data:
-                           text = message.get("text", "").strip()
-                           if not text:
-                              continue
-                           user = message.get("user_profile", {}).get("real_name") or message.get("user", "unknown")
-                           ts = message.get("ts", "")
-                           thread_ts = message.get("thread_ts", ts)
-                           is_reply = "parent_user_id" in message
-
-                           all_messages.append({
-                              "text": text,
-                              "filename": os.path.relpath(path, self.slack_dir),
-                              "user": user,
-                              "ts": ts,
-                              "thread_ts": thread_ts,
-                              "is_reply": is_reply
-                           })
-                  except (json.JSONDecodeError, FileNotFoundError) as e:
-                     print(f"Error loading {filename}: {e}")
-         print(f"Folder {subdir_path} done")
-      print(f"\nRead {len(all_messages)} messages from subfolders.")
-      return all_messages
-
-
-   def chunk_texts(self, messages: List[Dict]) -> List[Dict]:
-      #splits the loaded messages into smaller, more manageable chunks
-      text_splitter = RecursiveCharacterTextSplitter(
-      chunk_size = self.chunk_size,
-      chunk_overlap = self.chunk_overlap,
-      length_function = len,
-      )
-
-      chunks = []
-      for message in messages:
-         text_chunks = text_splitter.split_text(message["text"])
-         for chunk in text_chunks:
-            chunks.append({
-               "text": chunk,
-               "filename": message["filename"],
-               "user": message["user"],
-               "ts": message["ts"],
-               "thread_ts": message["thread_ts"],
-               "is_reply": message["is_reply"]
-            })
-
-      return chunks
   
    def create_vector_store(self) -> Chroma:
       #creates a Chroma vector store from the text chunks with semantic embedding model
-      print(f"Loading embedding model: {self.embedding_model}")
-      print("Creating vector store...")
-      messages = self.load_messages()
-      chunks = self.chunk_texts(messages)
-      documents = [
-         Document(
-            page_content = chunk["text"],
-            metadata = {
-               "filename": chunk["filename"],
-               "user": chunk["user"],
-               "ts": chunk["ts"],
-               "thread_ts": chunk["thread_ts"],
-               "is_reply": chunk["is_reply"]
-            }
-         )
-         for chunk in chunks
-      ]
+      loader = SlackDirectoryLoader(self.slack_dir)
+      raw_documents = loader.load()
+      print(f"\n\nLoaded {len(raw_documents)} documents\n\n")
+
+      splitter = RecursiveCharacterTextSplitter(chunk_size = self.chunk_size, chunk_overlap = self.chunk_overlap)
+      documents = splitter.split_documents(raw_documents)
 
       self.vectorstore = Chroma.from_documents(documents = documents, embedding = self.embeddings, persist_directory = self.db_path)  #load db to 
       self.vectorstore.persist()  #keep it on disk, in this version, i think it persists it anyways
@@ -120,4 +50,4 @@ class SlackIndexer:
 
 class GoogleIndexer:
    def __init__(self):
-      pass
+      pass 
